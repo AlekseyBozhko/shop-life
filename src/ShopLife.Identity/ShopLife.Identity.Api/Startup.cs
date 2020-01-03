25a6c4714393d8bfead8a51ca874c9b54569004e
AlekseyBozhko <47057435+AlekseyBozhko@users.noreply.github.com>
2020-01-03 02:33:32 +0200

    fixed name Identity.Api

--- src/ShopLife.Identity/ShopLife.Identity.Api/Startup.cs
+++ src/ShopLife.Identity/ShopLife.Identity.Api/Startup.cs
@@ -1,3 +1,7 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -8,10 +12,13 @@ namespace ShopLife.Identity.Api
 {
     public class Startup
     {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
